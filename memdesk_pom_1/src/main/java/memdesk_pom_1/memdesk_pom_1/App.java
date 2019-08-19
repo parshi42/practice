4e5d338442b776eda43509803e4f199ef410af57
pnamani <pnamani@hydws102709.hyd.corp.int.untd.com>
2019-08-19 10:53:07 +0530

    Initial

---
+++ memdesk_pom_1/src/main/java/memdesk_pom_1/memdesk_pom_1/App.java
@@ -0,0 +1,13 @@
+package memdesk_pom_1.memdesk_pom_1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
